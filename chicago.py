dffe73686b459e608d316bed47f6fea57cb250e8
Annie Liemohn <aliemohn@umich.edu>
2024-11-21 13:41:42 -0500

    debugging parameter "page"

--- chicago.py
+++ chicago.py
@@ -6,18 +6,22 @@ import sqlite3
 import random
 
 #25 paintings
-def get_paintings():
+def get_paintings(page_lst):
 
     #limit = 25
     #page = random.randint(0, 100)
     #url =  f"https://api.artic.edu/api/v1/artworks?page={page}&limit={limit}"
-
-    url =  "https://api.artic.edu/api/v1/artworks"
-    params = {'artwork_type_title': 'Painting', 'page': random.randint(0, 100), 'limit': 25}
+    page = random.randint(0, 6)
+    if page not in page_lst:
+        url =  "https://api.artic.edu/api/v1/artworks"
+        params = {'page': page, 'limit': 100}
+        print(page)
+    else:
+        None
 
     paintings = []
 
-    response = requests.get(url, params = params)
+    response = requests.get(url, params=params)
 
     if response.status_code == 200:
         data = response.json()
@@ -49,44 +53,49 @@ def insert_paintings_into_chicago(paintings, cur, conn):
     
     # go through each painting
     for painting in paintings:
-        
-        #title
-        if painting['title']:
-            title = painting['title']
-        else:
-            title = None
-        
-        #date end
-        if painting['date_end']:
-            creation_date = painting['date_end']
-        else:
-            creation_date = None
-        
-        #width
-        if painting['dimensions_detail'][0]['width']:
-            width_cm = painting['dimensions_detail'][0]['width']
-        else:
-            width_cm = None
+        if painting['artwork_type_title'] == 'Painting':
+            print('this is a painting')
+            
+            #title
+            if painting['title']:
+                title = painting['title']
+            else:
+                title = None
+            
+            #date end
+            if painting['date_end']:
+                creation_date = painting['date_end']
+            else:
+                creation_date = None
+            
+            #width
+            if painting['dimensions_detail']:
+                if painting['dimensions_detail'][0].get('width'):
+                    width_cm = painting['dimensions_detail'][0]['width']
+                else:
+                    width_cm = None
+            else:
+                width_cm = None
 
-        #add data into columns in database
-        cur.execute('''
-                    INSERT OR IGNORE INTO Chicago
-                    (title, creation_year, width_cm)
-                    VALUES (?,?,?)
-                    ''',
-                    (title, creation_date, width_cm)
-                    )
-        
-        # add painting to list to keep track of new paintings added, but only if inserted
-        if cur.rowcount > 0:
-                new_paintings.append(painting)
-                print(f"added painting title: '{title}' to database")
-        else:
-            print(f"painting '{title}' is already in database")
+            #add data into columns in database
+            cur.execute('''
+                        INSERT OR IGNORE INTO Chicago
+                        (title, creation_year, width_cm)
+                        VALUES (?,?,?)
+                        ''',
+                        (title, creation_date, width_cm)
+                        )
+            
+            # add painting to list to keep track of new paintings added, but only if inserted
+            if cur.rowcount > 0:
+                    new_paintings.append(painting)
+                    print(f"added painting title: '{title}' to database")
+            else:
+                print(f"painting '{title}' is already in database")
 
-        #ensuring only 25 are added at a time
-        if len(new_paintings) == 25:
-            break
+            #ensuring only 25 are added at a time
+            if len(new_paintings) == 25:
+                break
 
     print(f"Added {len(new_paintings)} new paintings to database")
 
@@ -94,10 +103,13 @@ def insert_paintings_into_chicago(paintings, cur, conn):
     return new_paintings
 
 def main():
+    page = random.randint(0, 6)
+    page_lst = []
+    
+
     # make the db, make the table
     cur, conn = create_database("Chicago.db")
     create_chicago_table(cur, conn)
-    
 
     # print total number of paintings in db right now
     cur.execute("SELECT COUNT(*) FROM Chicago")
